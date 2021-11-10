bc8079ff001132ba12cd32d98fb3e140ba0b0e0e
노서영 <hello.syeong@netivered.com>
2021-11-10 11:16:39 +0900

    ~04. create account, (social) log in, log out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,21 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
 
 const AppRouter = ({isLoggedIn}) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Routes>
                 {isLoggedIn ? (
-                    <Route exact path="/" element={<Home />} />
+                    <>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/profile" element={<Profile />} />
+                    </>
                 ) : (
-                    <Route exact path="/" element={<Auth />} />
+                    <Route path="/" element={<Auth />} />
                 )}
             </Routes>
         </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,18 @@
-const Profile = () => <span>Profile</span>;
+import { authService } from "fbase";
+import { useNavigate } from "react-router";
+
+const Profile = () => {
+    const navigate = useNavigate();
+    const onLogOutClink = () => {
+        authService.signOut();
+        navigate("/");
+    };
+
+    return (
+        <>
+            <button onClick={onLogOutClink}>Log Out</button>
+        </>
+    )
+};
 
 export default Profile;
